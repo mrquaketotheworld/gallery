5e45ff13eb906bcd8452443eb61d7930bf8606cd
mrquaketotheworld <newrushiflyer@gmail.com>
2024-08-11 19:01:13 +0300

    Add dynamic FAQ page

--- controllers/static.go
+++ controllers/static.go
@@ -10,3 +10,26 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "How are you?",
+			Answer:   "Good!",
+		},
+		{
+			Question: "How old are you?",
+			Answer:   "33",
+		},
+		{
+			Question: "What are you doing?",
+			Answer:   "Programming",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
--- views/template.go
+++ views/template.go
@@ -15,8 +15,8 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing FS: %v", err)
 	}
@@ -41,7 +41,7 @@ type Template struct {
 
 func (t Template) Execute(w http.ResponseWriter, data any) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "Error executing template", http.StatusInternalServerError)
