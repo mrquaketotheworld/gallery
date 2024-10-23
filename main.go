7d91a92c81ff0c2a4ae40b790b8f13beb4c93903
mrquaketotheworld <newrushiflyer@gmail.com>
2024-10-23 21:02:54 +0300

    Add sign in page

--- main.go
+++ main.go
@@ -32,8 +32,10 @@ func main() {
 		UserService: &userService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
