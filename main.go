c4ccda07b15c8d8ae307730cea8ca9c0ab2412ea
mrquaketotheworld <newrushiflyer@gmail.com>
2024-10-31 17:27:08 +0300

    Add cookie & session logic

--- main.go
+++ main.go
@@ -29,8 +29,12 @@ func main() {
 	userService := models.UserService{
 		DB: db,
 	}
+	sessionService := models.SessionService{
+		DB: db,
+	}
 	usersC := controllers.Users{
 		UserService: &userService,
+		SessionService: &sessionService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
