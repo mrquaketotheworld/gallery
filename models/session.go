8b4459bdd4cc92d76366167c4463f5b576c32711
mrquaketotheworld <newrushiflyer@gmail.com>
2024-11-01 18:11:53 +0300

    Add get User session

--- models/session.go
+++ models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/mrquaketotheworld/gallery/rand"
@@ -19,7 +21,7 @@ type Session struct {
 }
 
 type SessionService struct {
-	DB *sql.DB
+	DB            *sql.DB
 	BytesPerToken int
 }
 
@@ -33,12 +35,53 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		return nil, fmt.Errorf("create: %w", err)
 	}
 	session := Session{
-		UserID: userID,
-		Token: token,
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
+	}
+
+	row := ss.DB.QueryRow(`
+	UPDATE sessions
+	SET token_hash = $2
+	WHERE user_id = $1
+	RETURNING id;
+	`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err == sql.ErrNoRows {
+		row = ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id, token_hash)
+		VALUES ($1, $2) RETURNING id;
+		`, session.UserID, session.TokenHash)
+		err = row.Scan(&session.ID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
 	}
 	return &session, nil
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`
+		SELECT user_id FROM sessions WHERE token_hash = $1;
+		`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	row = ss.DB.QueryRow(`
+	SELECT email, password_hash
+	FROM users WHERE id=$1;
+	`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	return &user, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
